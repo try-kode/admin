b348cb841e9c8910d656b3c40d8023f980e6d1d8
try-kode <leangtrysam656@gmail.com>
2025-09-18 22:53:27 +0700

    create login form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('index');
 });
+
+Route::get('/login', [AuthController::class, 'login']);
