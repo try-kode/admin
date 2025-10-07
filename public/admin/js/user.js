ce7c9637fd6768b8e777acb892989aca8c4ea1d9
try-kode <leangtrysam656@gmail.com>
2025-10-07 22:18:13 +0700

    fix change password

---
+++ public/admin/js/user.js
@@ -0,0 +1,171 @@
+$(document).ready(function(){
+    fetchAllUsers();
+})
+$(".btnCreateUser").click(function(e){
+    // alert('you clicked')
+    e.preventDefault()
+    const username = $("#username").val()
+    const email = $("#email").val()
+    const password = $("#password").val()
+    const role = $("#role").val()
+    const formData = {
+        username: username,
+        email: email,
+        password: password,
+        role: role
+    }
+    console.log(formData)
+
+        $.ajax({
+            url: "https://api.viheakode.online/api/v1/user/create",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(formData),
+            success: function(response) {
+                toastr.success('<b>Notification</b><p>'+response.message+'</p>')
+                setTimeout(function() {
+                    window.location.href = "/admin/user/create";
+                }, 1500);
+            },
+        })
+
+});
+
+// function deleteUser(uuid){
+//     alert("you clicked")
+// }
+
+
+function fetchAllUsers(){
+    $.ajax({
+        url: "/admin/ajxFetchAllUsers",
+        method: "GET",
+        success: function(response) {
+            let rows = response.data
+            let cardUsers = ``
+            let idx = 0
+            rows.forEach(element => {
+                idx++
+                cardUsers +=`
+                    <div class="col-lg-3">
+                        <div class='card'>
+                            <div class="card-body">
+                                <h5><i class="fas fa-user"></i></h5>
+                                <h3>${element.username}</h3>
+                                <p>${element.role}</p>
+                                
+                                
+                            </div>
+                            <div class="card-footer">
+                                <a href="" class="btn btn-info btn-flat"><i class="fas fa-eye"></i></a>
+                                <a href="javascript:void(0)" class="btn btn-danger btnDeleteUser" recordId="${ element.userId }"><i class="fas fa-trash-alt"></i></a>
+                                <a href="" class="btn btn-warning btn-flat"><i class="fas fa-edit"></i></a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                `
+            })
+            $("#cardUsers").html(cardUsers)
+        },
+    })
+}
+
+
+// function fetchAllUsers(){
+//     $.ajax({
+//         url: "https://api.viheakode.online/api/v1/user/",
+//         method: "GET",
+//         success: function(response) {
+//             let rows = response.data
+//             let cardUsers = ``
+//             let idx = 0
+//             rows.forEach(element => {
+//                 idx++
+//                 cardUsers +=`
+//                     <div class="col-lg-3">
+//                         <div class='card'>
+//                             <div class="card-body">
+//                                 <h5><i class="fas fa-user"></i></h5>
+//                                 <h3>${element.username}</h3>
+//                                 <p>${element.role}</p>
+                                
+                                
+//                             </div>
+//                             <div class="card-footer">
+//                                 <a href="" class="btn btn-info btn-flat"><i class="fas fa-eye"></i></a>
+//                                 <a href="javascript:void(0)" class="btn btn-danger btnDeleteUser" recordId="${ element.userId }"><i class="fas fa-trash-alt"></i></a>
+//                                 <a href="" class="btn btn-warning btn-flat"><i class="fas fa-edit"></i></a>
+//                             </div>
+                            
+//                         </div>
+//                     </div>
+//                 `
+//             })
+//             $("#cardUsers").html(cardUsers)
+//         },
+//     })
+// }
+
+$(document).on("click", ".btnDeleteUser", function(e){
+    
+    const id = $(this).attr("recordId")
+
+    $.ajax({
+            url: "https://api.viheakode.online/api/v1/user/"+id,
+            method: "DELETE",
+            success: function(response) {
+                toastr.success('<b>Notification</b><p>'+response.message+'</p>')
+                fetchAllUsers();
+            },
+        })
+})
+
+$(document).on("click", ".btnLogout", function(){
+    $.ajax({
+        url: $(this).attr("actionURL"),
+        method: "POST",
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        success: function(response){
+            if(response.success == true){
+                console.log(response)
+                toastr.success('<b>Notification</b><p>'+response.message+'</p>')
+                setTimeout(function() {
+                    window.location.href = response.redirectURL
+                }, 1500)
+            }
+        }
+    })
+})
+
+$(document).on("click", ".btnChangePassword", function(e){
+    e.preventDefault();
+    $.ajax({
+        url: $(this).attr("actionURL"),
+        method: "POST",
+        data: {
+            currentPassword: $("#currentPassword").val(),
+            password: $("#password").val(),
+            password_confirmation: $("#password_confirmation").val()
+            
+        },
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        success: function(response){
+            if(response.success == true){
+                toastr.success('<b>Notification</b><p>'+response.message+'</p>')
+                setTimeout(function() {
+                    window.location.href = response.redirectURL
+                }, 1500)
+            } else {
+                toastr.error('<b>Notification</b><p>'+response.message+'</p>')
+            }
+        },
+        error: function(error){
+            toastr.error('<b>Notification</b><p>'+error.responseJSON.message+'</p>')
+        }
+    })
+})
